61023f507dba7150862c6160b8eacddd6c84bc50
AstroB <astrob@pop-os.localdomain>
2023-12-06 16:47:06 +0100

    feat: first commit from cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A diabetes classification machine learning project',
+    author='huwb',
+    license='MIT',
+)
